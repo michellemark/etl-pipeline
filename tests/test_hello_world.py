2224c3c763b70a41d569d547691c8303ce968c0a
Michelle Mark <web-developer@michellemark.me>
2025-02-21 11:00:37 -0500

    rename original prefect example to hello world

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 import pytest
 from prefect.testing.utilities import prefect_test_harness
 
-from etl.data_import_pipeline import hello_world
+from etl.hello_world import hello_world
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -13,24 +13,24 @@ def prefect_test_fixture():
 
 
 def test_hello_world_flow_default_args():
-    with patch('etl.data_import_pipeline.print_hello') as mock_print_hello:
-        with patch('etl.data_import_pipeline.print_goodbye') as mock_print_goodbye:
+    with patch('etl.hello_world.print_hello') as mock_print_hello:
+        with patch('etl.hello_world.print_goodbye') as mock_print_goodbye:
             hello_world()
             mock_print_hello.assert_called_once_with("world")
             mock_print_goodbye.assert_not_called()
 
 
 def test_hello_world_flow_with_name():
-    with patch('etl.data_import_pipeline.print_hello') as mock_print_hello:
-        with patch('etl.data_import_pipeline.print_goodbye') as mock_print_goodbye:
+    with patch('etl.hello_world.print_hello') as mock_print_hello:
+        with patch('etl.hello_world.print_goodbye') as mock_print_goodbye:
             hello_world("Michelle")
             mock_print_hello.assert_called_once_with("Michelle")
             mock_print_goodbye.assert_not_called()
 
 
 def test_hello_world_flow_with_name_and_goodbye():
-    with patch('etl.data_import_pipeline.print_hello') as mock_print_hello:
-        with patch('etl.data_import_pipeline.print_goodbye') as mock_print_goodbye:
+    with patch('etl.hello_world.print_hello') as mock_print_hello:
+        with patch('etl.hello_world.print_goodbye') as mock_print_goodbye:
             hello_world("Michelle", True)
             mock_print_hello.assert_called_once_with("Michelle")
             mock_print_goodbye.assert_called_once_with("Michelle")
