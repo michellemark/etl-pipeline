f60fc648de8192d52b8be6fb92dc9f2b6cd450bb
Michelle Mark <web-developer@michellemark.me>
2025-02-26 00:34:54 -0500

    add pydantic validation models

---
+++ etl/validation_models.py
@@ -0,0 +1,46 @@
+from decimal import Decimal
+from typing import List
+
+from pydantic import BaseModel, Field
+
+from etl.constants import OPEN_NY_EARLIEST_YEAR
+
+
+class MunicipalityAssessmentRatio(BaseModel):
+    rate_year: int = Field(
+        ge=OPEN_NY_EARLIEST_YEAR,
+        description="Rate Year of assessment ratio"
+    )
+    swis_code: str = Field(
+        min_length=6,
+        serialization_alias="municipality_code"
+    )
+    type: str = Field(
+        min_length=4,
+        description="Type of Municipality ie: City, Town, Village, County",
+        exclude=True
+    )
+    county_name: str = Field(
+        min_length=6,
+        description="Name of County of Municipality",
+        exclude=True
+    )
+    municipality_name: str = Field(
+        min_length=2,
+        description="Name of Municipality"
+    )
+    village_name: str | None = Field(
+        None,
+        description="Name of Village, usually not present in data",
+        exclude = True
+    )
+    residential_assessment_ratio: Decimal = Field(
+        decimal_places=2,
+        description="Ratio of assessed value compared to full value of that property"
+    )
+
+    class Config:
+        # Ensure Decimal values are dumped as floats
+        json_encoders = {
+            Decimal: lambda v: float(v)
+        }
